251c5934226b0b444fee37590cc9ead7a1611bcc
Sanket Deb <imsanketdeb20@gmail.com>
2024-08-24 19:58:39 +0530

    creating your own npx create react app

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", {id: "parent"}, [
     React.createElement("div", {id: "child1"}, [
-        React.createElement("h1", {}, "I am a h1 tag"),
-        React.createElement("h2", {}, "I am a h2 tag")
+        React.createElement("h1", {}, "I am you"),
+        React.createElement("h2", {}, "I can go up ")
     ]), 
     React.createElement("div", {id: "child2"}, [
         React.createElement("h1", {}, "I am a h1 tag"),
