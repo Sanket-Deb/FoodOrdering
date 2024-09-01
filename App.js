c7d4a3ab2c367da0a532accf05915dd3f158641b
Sanket Deb <imsanketdeb20@gmail.com>
2024-09-01 17:41:18 +0530

    React Element

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child1"}, [
-        React.createElement("h1", {}, "I am you"),
-        React.createElement("h2", {}, "I can go up ")
-    ]), 
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "I am a h1 tag"),
-        React.createElement("h2", {}, "I am a h2 tag")
-    ])
-]);
+
+const Heading = <h1 id="heading" className="head">Namaste Sanket</h1>
+
+console.log(Heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(Heading);
