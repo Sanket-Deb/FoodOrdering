98478afead3514eb6ace90464f223e2afe842eda
Sanket Deb <imsanketdeb20@gmail.com>
2024-09-03 00:15:39 +0530

    Food App Initial Commit

--- App.js
+++ App.js
@@ -2,25 +2,60 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const Title = () => (
-    <h1 id="heading" className="head">
-        Namaste Sanket
-    </h1>
-)
-const title = (
-    <h1 id="heading" className="head">
-        Namaste Sanket2
-    </h1>
-)
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="	https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+            
+        </div>)
+}
 
-const HeadingComponent = () => (
-    <div id="headingContainer">
-        <Title />
-        {title}
-        <h1 className="heading">Namaste Sanket Functional Component</h1>
-    </div>
-)
+const RestrauntCard = () => {
+    return (
+        <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+            <img
+             className="res-logo"
+                alt="res-logo"
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf" 
+            />
+            <h3>Rammu's Canteen</h3>
+            <h4>Biryani, Continental</h4>
+            <h4>4.2 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
+const Body = () => {
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestrauntCard />
+            </div>
+        </div>
+    )
+
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+        <Body />
+        </div>)
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
