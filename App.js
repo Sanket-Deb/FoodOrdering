d0cfb27a3b364a9fe8f5c2fc2aff263b8169ec4a
Sanket Deb <imsanketdeb20@gmail.com>
2024-09-02 20:31:08 +0530

    JS update

--- App.js
+++ App.js
@@ -2,10 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const Heading = <h1 id="heading" className="head">Namaste Sanket</h1>
+const Title = () => (
+    <h1 id="heading" className="head">
+        Namaste Sanket
+    </h1>
+)
+const title = (
+    <h1 id="heading" className="head">
+        Namaste Sanket2
+    </h1>
+)
 
-console.log(Heading);
+const HeadingComponent = () => (
+    <div id="headingContainer">
+        <Title />
+        {title}
+        <h1 className="heading">Namaste Sanket Functional Component</h1>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(Heading);
+root.render(<HeadingComponent/>);
