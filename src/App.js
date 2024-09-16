b5468de2f903f9bdc000931679b7449e3dcb302c
Sanket Deb <imsanketdeb20@gmail.com>
2024-09-16 22:32:35 +0530

    Implemented Lazy Loading for Grocery and About component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import RestaurantMenu from "./components/ResMenu";
+import Shimmer from "./components/shimmer";
+// import Grocery from "./components/Grocery";
+
+// Chunking
+// Code Spliting
+// Dynamic Bundling
+// Lazy loading
+// on demand loading
+// dynamic import
+
+const Grocery = lazy(() => import("./components/Grocery"));
+
+const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
   return (
@@ -28,11 +41,23 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About></About>,
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <About />
+          </Suspense>
+        ),
       },
       {
         path: "/contact",
         element: <Contact></Contact>,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>I'm coming pls wait for me</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurants/:resId",
---
+++ src/components/Grocery.js
@@ -0,0 +1,7 @@
+const Grocery = () => {
+  return (
+    <h1> Our Grocery Online Store with lots of child components within it</h1>
+  );
+};
+
+export default Grocery;
