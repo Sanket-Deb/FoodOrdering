04aad45929bc672637afd0309df232a9931db689
Sanket Deb <imsanketdeb20@gmail.com>
2024-09-17 21:02:37 +0530

    Tailwind CSS initial implementation

--- src/components/Body.js
+++ src/components/Body.js
@@ -42,17 +42,18 @@ const Body = () => {
     <Shimmer />
   ) : (
     <div className="body">
-      <div className="filter">
-        <div className="search">
+      <div className="flex">
+        <div className="search m-4 p-4">
           <input
             type="text"
-            className="search-box"
+            className="border border-solid border-black"
             value={searchText}
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
           ></input>
           <button
+            className="px-4 py-1 bg-green-100 m-4 rounded-lg"
             onClick={() => {
               console.log(searchText);
               const filteredRestaurants = listOfRestaurants.filter((res) =>
@@ -65,19 +66,21 @@ const Body = () => {
             Search
           </button>
         </div>
-        <button
-          className="filter-btn"
-          onClick={() => {
-            const filteredList = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4.3
-            );
-            setFilteredRestaurants(filteredList);
-          }}
-        >
-          Top Rated restaurants
-        </button>
+        <div className="search m-4 p-4 flex items-center">
+          <button
+            className="px-4 py-1 bg-gray-100 m-4 rounded-lg"
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4.3
+              );
+              setFilteredRestaurants(filteredList);
+            }}
+          >
+            Top Rated restaurants
+          </button>
+        </div>
       </div>
-      <div className="res-container">
+      <div className="flex flex-wrap justify-center">
         {filteredRestaurants?.map((restaurant) => (
           <Link
             key={restaurant.info.id}
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,26 +9,26 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between bg-pink-100 shadow-lg mb-2 sm:bg-yellow-50 lg:bg-green-50">
       <div className="logo-container">
         <img className="w-40" src={LOGO_URL} />
       </div>
-      <div className="nav-items">
+      <div className="flex items-center">
         <ul className="flex p-4 m-4">
-          <li>Online Status: {onlineStatus ? "✅" : "🚩"}</li>
-          <li>
+          <li className="px-4">Online Status: {onlineStatus ? "✅" : "🚩"}</li>
+          <li className="px-4  hover:text-blue-400">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-4  hover:text-blue-400">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="px-4  hover:text-blue-400">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li>
+          <li className="px-4  hover:text-blue-400">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li>Cart</li>
+          <li className="px-4">Cart</li>
           <button
             className="login"
             onClick={() => {
